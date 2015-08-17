c555bdab8860771ccbf4004d1f2212ba971aba66
Kyle King <kylerking@me.com>
2015-08-17 12:20:41 -0700

    add main js script

---
+++ main.js
@@ -0,0 +1,127 @@
+
+var player = 1;
+
+
+
+var chooseLetter = document.querySelectorAll(".square");
+
+//box 0  ////////////////////////////////////////////////
+chooseLetter[0].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click0").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click0").innerText="o";
+		}
+	player++;
+})
+
+//box 1  ///////////////////////////////////////////////
+chooseLetter[1].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click1").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click1").innerText="o";
+		}
+	player++;
+})
+
+//box 2  //////////////////////////////////////////////
+chooseLetter[2].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click2").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click2").innerText="o";
+		}
+	player++;
+})
+
+//box 3   //////////////////////////////////////////////
+chooseLetter[3].addEventListener("click",function(){
+if ( player % 2 === 0 ) {
+
+		document.getElementById("click3").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click3").innerText="o";
+		}
+	player++;
+})
+
+//box 4  ///////////////////////////////////////////////
+chooseLetter[4].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click4").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click4").innerText="o";
+		}
+	player++;
+})
+
+//box 5  ///////////////////////////////////////////////
+chooseLetter[5].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click5").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click5").innerText="o";
+		}
+	player++;
+})
+
+//box 6  ////////////////////////////////////////////////
+chooseLetter[6].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click6").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click6").innerText="o";
+		}
+	player++;
+})
+
+//box 7 /////////////////////////////////////////////////
+chooseLetter[7].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click7").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click7").innerText="o";
+		}
+	player++;
+})
+
+//box 8  /////////////////////////////////////////////////
+chooseLetter[8].addEventListener("click",function(){
+	if ( player % 2 === 0 ) {
+
+		document.getElementById("click8").innerText="x";
+
+		}else{ 
+
+		document.getElementById("click8").innerText="o";
+		}
+	player++;
+})
+
+	// document.querySelector("reset").addEventListener("click", function("reset"))
+
+
