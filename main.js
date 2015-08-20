c8c7b93bff55c4092abcff69da110306eaa93f80
Kyle King <kylerking@me.com>
2015-08-20 14:39:18 -0700

    update code on all files

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
+var turn=0;
+var squares = $(".square");
+console.log(squares);
+var player = " ";
+//calling all the values within .square (class)-->console log them
+var arr =	[
+			[0, 1, 2],
+			[3, 4, 5], 
+			[6, 7, 8], 
+			[0, 3, 6], 
+			[1, 4, 7], 
+			[2, 5, 8], 
+			[0, 4, 8], 
+			[2, 4, 6]
+			]	
 
+$(".square").on("click", function(){
+	if($(this).text() !== ""){
+		alert("Choose another box!");
+	} else {	
+		if (turn===1) {
+			$(this).html("X");
+			$(this).addClass("clickedX");
+			player="X";
+		} else {
+			$(this).html("O");
+			$(this).addClass("clickedY")
+			player="O";
+		}
+		turn=turn===0?1:0;
+		checkForWinner();
+	}
 
-$("h1").on("click", function()){
+});
+
+$("#reset").on("click", function(){
 	console.log(this);
+	$(".square").empty();
+});
 
+function checkForWinner(){
+	console.log("Yay")
+	 $.each(arr, function(i, v){
+	 	console.log(player)
+	 	if(squares[v[0]].innerHTML===squares[v[1]].innerHTML && squares[v[1]].innerHTML===squares[v[2]].innerHTML && squares[v[2]].innerHTML!== ""){
+			alert("There is a winner!");
+	 	}
+	});
 }
+checkForWinner();
+
+
 
 
 
